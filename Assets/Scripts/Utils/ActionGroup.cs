b28e2f42756b9de0bd0a2538b5701ee031253d9f
guins94 <guins94@hotmail.com>
2024-07-02 18:30:31 -0300

    Action Group Script Created

---
+++ Assets/Scripts/Utils/ActionGroup.cs
@@ -0,0 +1,11 @@
+using System;
+
+/// <summary>
+/// Public Game Used Actions
+/// </summary>
+public static class ActionGroup
+{
+    public static Action<int> CoinCollected = null;
+    public static Action CoinDeposit = null;
+    public static Action OnGameStart = null;
+}
