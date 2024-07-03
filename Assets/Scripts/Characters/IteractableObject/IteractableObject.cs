78463f6176fed1640f59b66dab56ad98b391cacf
guins94 <guins94@hotmail.com>
2024-07-02 22:56:28 -0300

    Iteractable Object Script Created

---
+++ Assets/Scripts/Characters/IteractableObject/IteractableObject.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public abstract class IteractableObject : MonoBehaviour
+{
+    [Header("Iteractable Object Components References")]
+    [SerializeField] Collider2D iteractableAreaCollider2D;
+    [SerializeField] SpriteRenderer ChatBoxSpriteRenderer;
+
+    // Cached Components
+    public bool canInteract = false;
+
+    public void Start()
+    {
+        GameManager.PlayerInstance.IteractWithObject += IteractionHandler;
+    }
+
+    public void IteractionHandler()
+    {
+        if (canInteract) IteractionEffect();
+    }
+
+    public abstract void IteractionEffect();
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //iteractableAreaCollider2D.enabled = false;
+            EnableIteractionChatBoxHandler();
+            canInteract = true;
+        } 
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //iteractableAreaCollider2D.enabled = false;
+            DisableIteractionChatBoxHandler();
+            canInteract = false;
+        } 
+    }
+
+    private void EnableIteractionChatBoxHandler()
+    {
+        ChatBoxSpriteRenderer.enabled = true;
+    }
+
+    private void DisableIteractionChatBoxHandler()
+    {
+        ChatBoxSpriteRenderer.enabled = false;
+    }
+}
